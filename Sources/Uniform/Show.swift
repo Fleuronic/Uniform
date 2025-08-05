23c716a6f735117da23a9092b232f6f3b83d8d83
Jordan Kay <jordan@fleuronic.com>
2025-08-05 19:48:26 -0400

    Add show validity check

--- Sources/Uniform/Show.swift
+++ Sources/Uniform/Show.swift
@@ -2,11 +2,20 @@ import struct DrumKit.Show
 
 public extension Show {
 	static func name(for record: String) -> String {
-		record
+		let name = record
+			.replacingOccurrences(of: " - ", with: " — ")
 			.replacingOccurrences(of: "--", with: "—")
-			.replacingOccurrences(of: "-", with: "—")
 			.replacingOccurrences(of: "?", with: "—")
 			.replacingOccurrences(of: "'", with: "’")
 			.replacingOccurrences(of: "&amp;", with: "&")
+		return name.components(separatedBy: " presented by ").first!
+	}
+
+	static func isValid(with record: String)  -> Bool {
+		!record.contains("SoundSport") &&
+			!record.contains("Battle") &&
+			!record.contains("Virtual") &&
+			!record.contains("Rain") &&
+			!record.contains("Mini Corps")
 	}
 }
