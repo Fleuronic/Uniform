844c4fff62ae17a8346c424ee5c5fb65667d4537
Jordan Kay <jordan@fleuronic.com>
2025-08-05 17:59:20 -0400

    Update circuit

--- Sources/Uniform/Circuit.swift
+++ Sources/Uniform/Circuit.swift
@@ -1,18 +1,33 @@
 import struct DrumKit.Circuit
 
 public extension Circuit {
+	static func abbreviation(for name: String) -> String {
+		let abbreviation = name.components(separatedBy: "/").first!
+
+		return switch abbreviation {
+		case "", "DCI Division II & III": "DCI"
+		case "American Legion": "AL"
+		case "DCA-C", "DCA-Central": "DCA Central"
+		case "DCG": "DMG"
+		case "DCA-S", "DCI-South": "DCI South"
+		case "DCI-Pacific": "DCI Pacific"
+		default: abbreviation
+		}
+	}
+
 	static func name(for abbreviation: String) -> String {
 		switch abbreviation {
+		case "AL": "American Legion"
 		case "DCI": "Drum Corps International"
 		case "DCA": "Drum Corps Associates"
+		case "CAMQ": "Circuit des associations musicales du Québec"
+		case "FAMQ": "Fédération des Associations Musicales du Québec"
 		case "DCM": "Drum Corps Midwest"
 		case "DCH": "Drum Corps Holland"
 		case "DCJ": "Drum Corps Japan"
 		case "DMG": "Dutch Music Games"
-		case "AL": "American Legion"
+		case "MCA": "Mini Corps Associates"
 		case "VFW": "Veterans of Foreign Wars"
-		case "CAMQ": "Circuit des associations musicales du Québec"
-		case "FAMQ": "Fédération des Associations Musicales du Québec"
 		default: abbreviation
 		}
 	}
