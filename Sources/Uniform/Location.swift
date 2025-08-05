d6e68ec7207823a3038f4ee2d3e2ef08018518a6
Jordan Kay <jordan@fleuronic.com>
2025-08-05 17:13:27 -0400

    Add location info

---
+++ Sources/Uniform/Location.swift
@@ -0,0 +1,19 @@
+import struct DrumKit.Location
+
+public extension Location {
+	static func info(forLocationNamed name: String) -> (String, String, String)? {
+		guard 
+			case let components = name.split(separator: " "),
+			let stateIndex = (components.firstIndex { $0.allSatisfy(\.isUppercase) }) else { return nil }
+		
+		let city = components[0..<stateIndex].joined(separator: " ")
+		let state = String(components[stateIndex]).uppercased()
+		let country = if stateIndex == components.count - 1 {
+			"United States"
+		} else {
+			components[(stateIndex + 1)...].joined(separator: " ")
+		}
+
+		return (city, state, country)
+	}
+}
