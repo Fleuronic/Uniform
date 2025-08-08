8bf9aad806695f68e9d1242bbf58c1952c3af8d7
Jordan Kay <jordan@fleuronic.com>
2025-08-08 12:15:44 -0400

    Update address

--- Sources/Uniform/Address.swift
+++ Sources/Uniform/Address.swift
@@ -3,6 +3,14 @@ import struct DrumKit.Address
 public extension Address {
 	static func info(for records: [String]) -> (String, String) {
 		let streetAddress = records[0]
+			.replacingOccurrences(of: ".", with: "")
+			.replacingOccurrences(of: "Road", with: "Rd")
+			.replacingOccurrences(of: "Street", with: "St")
+			.replacingOccurrences(of: "Avenue", with: "Ave")
+			.replacingOccurrences(of: "Boulevard", with: "Blvd")
+			.replacingOccurrences(of: "Lane", with: "Ln")
+			.replacingOccurrences(of: "Highway", with: "Hwy")
+			.replacingOccurrences(of: "Route", with: "Rte")
 		let zipCode = records[1].components(separatedBy: " ").last!
 		return (streetAddress, zipCode)
 	}
