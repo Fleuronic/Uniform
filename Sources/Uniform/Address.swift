669f16c7d626995b6c92bd5d5044a3074ba8fadd
Jordan Kay <jordan@fleuronic.com>
2025-08-08 11:39:50 -0400

    Update address

--- Sources/Uniform/Address.swift
+++ Sources/Uniform/Address.swift
@@ -3,7 +3,7 @@ import struct DrumKit.Address
 public extension Address {
 	static func info(for records: [String]) -> (String, String) {
 		let streetAddress = records[0]
-		let zipCode = records[1]
+		let zipCode = records[1].components(separatedBy: " ").last!
 		return (streetAddress, zipCode)
 	}
 }
