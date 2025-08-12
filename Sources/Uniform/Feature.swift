b33c4eb260e8c1777b7ab3187ce05d7741ef9ff7
Jordan Kay <jordan@fleuronic.com>
2025-08-12 18:00:06 -0400

    Add feature

---
+++ Sources/Uniform/Feature.swift
@@ -0,0 +1,17 @@
+import struct DrumKit.Feature
+
+public extension Feature {
+	static func name(for record: String) -> String? {
+		let features = [
+			"Blue Knights Awards Presentation",
+			"Encore",
+			"Gates Open",
+			"Intermission",
+			"Retreat",
+			"Scores Announced",
+			"Welcome & National Anthem"
+		]
+		
+		return features.contains(record) ? record : nil
+	}
+}
