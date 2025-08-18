f0165f439ee51a90606e8f22e63801b6306c5dd9
Jordan Kay <jordan@fleuronic.com>
2025-08-18 17:06:10 -0400

    Update corps

--- Sources/Uniform/Corps.swift
+++ Sources/Uniform/Corps.swift
@@ -1,8 +1,22 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import MemberwiseInit
+import DrumKit.Corps
 
 @MemberwiseInit(.public)
 public struct Corps: Equatable, Sendable {
 	public let name: String
+}
+
+// MARK: -
+public extension DrumKit.Corps {
+	typealias Info = (String, String)
+
+	static func info(for record: String) -> Info {
+		let components = record.components(separatedBy: " - ")
+		let name = components[0]
+		let location = components[1]
+
+		return (name, location)
+	}
 }
