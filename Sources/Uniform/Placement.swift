963b62b248425d742042472e43f03a5aa36ffee0
Jordan Kay <jordan@fleuronic.com>
2025-09-25 17:22:14 -0400

    Update placement

--- Sources/Uniform/Placement.swift
+++ Sources/Uniform/Placement.swift
@@ -5,6 +5,8 @@ public extension Placement {
 		switch record {
 		case "High School 57": "Beijing 57 High School"
 		case "Calgary Stetson Show Band": "Calgary Stetson Showband"
+		case "Blue Devils \"B\"": "Blue Devils B"
+		case "Blue Devils \"C\"": "Blue Devils C"
 		default: record
 		}
 	}
