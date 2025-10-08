4abf88b998489c48ea2f1de34262bf7079fc02d9
Jordan Kay <jordan@fleuronic.com>
2025-10-08 12:03:07 -0400

    Update division

--- Sources/Uniform/Division.swift
+++ Sources/Uniform/Division.swift
@@ -4,6 +4,7 @@ public extension Division {
 	static func name(for record: String) -> String {
 		var name = record
 			.replacingOccurrences(of: "All Age", with: "All-Age")
+			.replacingOccurrences(of: "A Class", with: "Class A")
 
 		name = ["Open", "World", "All-Age"].contains(name) ? name + " Class" : name
 
