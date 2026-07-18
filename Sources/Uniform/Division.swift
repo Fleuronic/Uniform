b583f64332b75fa9b29d8dc3100649d4862246b5
Jordan Kay <jordan@fleuronic.com>
2026-07-18 11:35:09 -0400

    Update division

--- Sources/Uniform/Division.swift
+++ Sources/Uniform/Division.swift
@@ -8,7 +8,9 @@ public extension Division {
 			.replacingOccurrences(of: "All Age", with: "All-Age")
 			.replacingOccurrences(of: "A Class", with: "Class A")
 
-		name = ["Open", "World", "All-Age", "International"].contains(name) ? name + " Class" : name
+		name = [
+			"Open", "World", "All-Age", "International", "Junior", "Premier"
+		].contains(name) ? name + " Class" : name
 
 		name = switch name {
 		case "A", "DCA A": "Class A"
